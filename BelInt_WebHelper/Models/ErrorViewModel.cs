4154088b9a8b35a06ea52b5035dd3aa7a6ec291f
Иван <myakivan@gmail.com>
2022-04-07 18:59:19 +0300

    Добавьте файлы проекта.

---
+++ BelInt_WebHelper/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BelInt_WebHelper.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
