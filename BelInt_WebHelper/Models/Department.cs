b5f0a993a9437ec89c5665ed8a26838f2958c588
Иван <myakivan@gmail.com>
2022-04-12 00:35:18 +0300

    Дополнил данными Объекты Авторизации + залил документацию по учреждению (шаблоны для формирования и данные в БД)

--- BelInt_WebHelper/Models/Department.cs
+++ BelInt_WebHelper/Models/Department.cs
@@ -6,6 +6,8 @@ namespace BelInt_WebHelper.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int? OrganisationId { get; set; }
+        public Organisation Organisation { get; set; }
         public List<User> Users { get; set; }
         public Department()
         {
