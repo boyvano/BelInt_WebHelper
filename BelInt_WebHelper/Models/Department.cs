084ff6d7aac4274ad98469f69aa431bf06c615b3
Иван <myakivan@gmail.com>
2022-04-11 23:13:35 +0300

    Положено начало : создал первую версию Класса Пользователи

---
+++ BelInt_WebHelper/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace BelInt_WebHelper.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<User> Users { get; set; }
+        public Department()
+        {
+            Users = new List<User>();
+        }
+    }
+}
