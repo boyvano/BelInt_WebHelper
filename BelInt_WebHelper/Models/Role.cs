1dfc61f8e86a0fab4f55c319fce51b304edd29bf
Иван <myakivan@gmail.com>
2022-04-25 23:27:09 +0300

    добавил атрибуты в класс, чтобы не забыть, как это делается

--- BelInt_WebHelper/Models/Role.cs
+++ BelInt_WebHelper/Models/Role.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BelInt_WebHelper.Models
 {
     public class Role
     {
+        [Required]
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public List<User> Users { get; set; }
