309f6856b43ea2ba1ee73b10d12347f96fb12504
Иван <myakivan@gmail.com>
2022-05-17 13:26:54 +0300

    90% сериализации готово

--- BelInt_WebHelper/Models/DataModels/GanerateContracts.cs
+++ BelInt_WebHelper/Models/DataModels/GanerateContracts.cs
@@ -41,27 +41,27 @@ WordprocessingDocument.Create(pathToDocFile,
                 foreach (Row r in wsPart.Worksheet.Descendants<Row>())
                 {
                     i++;
-                    if (r.Elements<Cell>().ElementAt(0).DataType != null)
+                    if (r.RowIndex < wsPart.Worksheet.Descendants<Row>().Count() - 2)
                     {
                         try
                         {
                             contracts.Add(new Contract()
                             {
                                 RowNomer = int.Parse(r.Elements<Cell>().ElementAt(0).CellValue.Text),
-                                Date = DateTime.Parse(r.Elements<Cell>().ElementAt(1).CellValue.Text),
-                                ContractId = r.Elements<Cell>().ElementAt(2).CellValue.Text,
-                                CompanyName = r.Elements<Cell>().ElementAt(3).CellValue.Text,
-                                CurrencyContractId = r.Elements<Cell>().ElementAt(4).CellValue.Text,
-                                RegDateCurrContract = DateTime.Parse(r.Elements<Cell>().ElementAt(5).CellValue.Text),
+                                //Date = DateTime.Parse(r.Elements<Cell>().ElementAt(1).CellValue.Text),
+                                ContractId = ReadExcelCell(r.Elements<Cell>().ElementAt(2), wbPart),
+                                CompanyName = ReadExcelCell(r.Elements<Cell>().ElementAt(3), wbPart),
+                                CurrencyContractId = ReadExcelCell(r.Elements<Cell>().ElementAt(4), wbPart),
+                                //RegDateCurrContract = DateTime.Parse(r.Elements<Cell>().ElementAt(5).CellValue.Text),
                                 SummaryPayment = double.Parse(r.Elements<Cell>().ElementAt(6).CellValue.Text),
-                                ContractCurrency = r.Elements<Cell>().ElementAt(7).CellValue.Text,
-                                ContractPayment = r.Elements<Cell>().ElementAt(8).CellValue.Text,
-                                CountryOfRegister = r.Elements<Cell>().ElementAt(9).CellValue.Text,
-                                DateOfContract = DateTime.Parse(r.Elements<Cell>().ElementAt(10).CellValue.Text),
-                                UserId = r.Elements<Cell>().ElementAt(11).CellValue.Text,
-                                PaymentType = r.Elements<Cell>().ElementAt(12).CellValue.Text,
-                                Reward = r.Elements<Cell>().ElementAt(13).CellValue.Text,
-                                IsYearWork = bool.Parse(r.Elements<Cell>().ElementAt(14).CellValue.Text),
+                                ContractCurrency = ReadExcelCell(r.Elements<Cell>().ElementAt(7), wbPart),
+                                ContractPayment = ReadExcelCell(r.Elements<Cell>().ElementAt(8), wbPart),
+                                CountryOfRegister = ReadExcelCell(r.Elements<Cell>().ElementAt(9), wbPart),
+                                //DateOfContract = DateTime.Parse(r.Elements<Cell>().ElementAt(10).CellValue.Text),
+                                UserId = ReadExcelCell(r.Elements<Cell>().ElementAt(11), wbPart),
+                                PaymentType = ReadExcelCell(r.Elements<Cell>().ElementAt(12), wbPart),
+                                Reward = ReadExcelCell(r.Elements<Cell>().ElementAt(13), wbPart),
+                                //IsYearWork = bool.Parse(r.Elements<Cell>().ElementAt(14).CellValue.Text),
                             });
 
                         }
@@ -73,8 +73,19 @@ WordprocessingDocument.Create(pathToDocFile,
                     }
                 }
             }
-
             return contracts;
+        }
+        private string ReadExcelCell(Cell cell, WorkbookPart workbookPart)
+        {
+            var cellValue = cell.CellValue;
+            var text = (cellValue == null) ? cell.InnerText : cellValue.Text;
+            if ((cell.DataType != null) && (cell.DataType == CellValues.SharedString))
+            {
+                text = workbookPart.SharedStringTablePart.SharedStringTable
+                    .Elements<SharedStringItem>().ElementAt(
+                        Convert.ToInt32(cell.CellValue.Text)).InnerText;
+            }
+            return (text ?? string.Empty).Trim();
         }
     }
 }
