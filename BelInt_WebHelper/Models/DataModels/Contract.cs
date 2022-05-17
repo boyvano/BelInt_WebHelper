aeb644035b5b718e6c303265ffccc6fa3443dcea
Miakenkaya Julia <julaymyak12@gmail.com>
2022-05-17 12:39:56 +0300

    Добавила аннотации с названиями колонок в contract.cs

--- BelInt_WebHelper/Models/DataModels/Contract.cs
+++ BelInt_WebHelper/Models/DataModels/Contract.cs
@@ -8,52 +8,70 @@ namespace BelInt_WebHelper.Models.DataModels
 {
     public class Contract
     {
-        //Порядковый номер строки.
+        // Порядковый номер строки.
+        [Display(Name = "Порядковый номер строки")]
         public int RowNomer { get; set; }
 
         // Дата регистрации договора на предприятии.
+        [Display(Name = "Дата регистрации договора")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
 
-        //Номер договора
+        // Номер договора
+        [Display(Name = "Номер договора")]
         public string ContractId { get; set; }
 
-        //Наименование предприятия
+        // Наименование предприятия
+        [Display(Name = "Предприятие")]
         public string CompanyName { get; set; }
 
-        //Номер валютного договора
+        // Номер валютного договора
+        [Display(Name = "Номер валютного договора")]
         public string CurrencyContractId { get; set; }
 
         // Дата регистрации договора в нац.банке
+        [Display(Name = "Дата регистрации договора в НБ")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime RegDateCurrContract { get; set; }
 
-        //Сумма договора
+        // Сумма договора
+        [Display(Name = "Сумма договора")]
         public double SummaryPayment { get; set; }
+
         // Валюта договора. Та, в которой клиенту согласована стоимость оказываемой услуги        
+        [Display(Name = "Валюта договора")]
         public string ContractCurrency { get; set; }
 
         // Валюта платежа. Та, в которой клиентосуществляет оплату        
+        [Display(Name = "Валюта платежа")]
         public string ContractPayment { get; set; }
+
         // Страна регистрации       
+        [Display(Name = "Страна регистрации")]
         public string CountryOfRegister { get; set; }
+
         // Срок договора. Или дата, или бессрочный
+        [Display(Name = "Срок договора")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime? DateOfContract { get; set; }
 
         // Исполнитель. Пользователь, ответственный за исполнение данного договора
+        [Display(Name = "Исполнитель")]
         public string UserId { get; set; }
 
         // Порядок оплаты. Предоплата (100%) или отсрочка - выбор шаблона договора
+        [Display(Name = "Порядок оплаты")]
         public string PaymentType { get; set; }
 
         // Вознаграждения. Ставка или процент - выбор шаблона договора
+        [Display(Name = "Вознаграждения")]
         public string Reward { get; set; }
 
-        // работа в текущем году. Оказывались ли услуги в текущем году или нет.
+        // Работа в текущем году. Оказывались ли услуги в текущем году или нет.
+        [Display(Name = "Работа в текущем году")]
         public bool IsYearWork { get; set; }
     }
 }
