6860f718f588bb3815ac95ddf8affb4467b89aa5
Иван <myakivan@gmail.com>
2022-05-27 17:16:45 +0300

    Готовлю работу с ролями.

--- BelInt_WebHelper/Models/ViewModels/CreateUserViewModel.cs
+++ BelInt_WebHelper/Models/ViewModels/CreateUserViewModel.cs
@@ -1,16 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System;
+using System.Collections.Generic;
 
 namespace BelInt_WebHelper.Models.ViewModels
 {
-    public class CreateUserViewModel
+    public class CreateUserViewModel : RegisterViewModel
     {
-        public string Email { get; set; }
-        public string Password { get; set; }
-
-        [DataType(DataType.DateTime)]
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
-        public DateTime? DateOfBirth { get; set; }
+        [Display(Name = "Роли")]
+        [System.ComponentModel.DataAnnotations.Schema.ForeignKey("Roles")]
+        public System.Collections.Generic.List<string> RolesId { get; set; }
+        public System.Collections.Generic.List<Microsoft.AspNetCore.Identity.IdentityRole> Roles { get; set; }
+        
     }
 
 }
--- BelInt_WebHelper/Models/ViewModels/EditUserViewModel.cs
+++ BelInt_WebHelper/Models/ViewModels/EditUserViewModel.cs
@@ -3,40 +3,8 @@ using System;
 
 namespace BelInt_WebHelper.Models.ViewModels
 {
-    public class EditUserViewModel
-    {
-        [Key]
+    public class EditUserViewModel: CreateUserViewModel
+    {        
         public string Id { get; set; }
-        [Required]
-        [Display(Name = "Фамилия")]
-        public string SurName { get; set; }
-
-        [Required]
-        [Display(Name = "Имя")]
-        public string FirstName { get; set; }
-
-        [Required]
-        [Display(Name = "Отчество")]
-        public string LastName { get; set; }
-
-        [DataType(DataType.Date)]
-        [Display(Name = "Дата рождения")]
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
-        public DateTime? DateOfBirth { get; set; }
-
-        [Required]
-        [Display(Name = "Логин")]
-        public string UserName { get; set; }
-
-        [Required]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
-
-        [Display(Name = "Отделение")]
-        public int DepartmentId { get; set; }
-
-        [Display(Name = "Должность")]
-        public string Position { get; set; }
-
     }
 }
